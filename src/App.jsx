4029077273613ca528eefa1c12fa8273a9f19bc7
Ravi Kumar Singh <77446236+ravisingh2811@users.noreply.github.com>
2024-03-07 17:51:51 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          GitHub Codespaces by ravi singh <span className="heart">♥️</span> React
         </p>
         <p className="small">
           Edit <code>src/App.jsx</code> and save to reload.
