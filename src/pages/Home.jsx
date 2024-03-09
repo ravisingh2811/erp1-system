5dcf0f11625d117d23e3245ec1b44e235fc51bc4
Ravi Kumar Singh <77446236+ravisingh2811@users.noreply.github.com>
2024-03-09 08:26:41 +0000

    done with upper grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
 
 function Home() {
   return (
@@ -16,41 +16,69 @@ function Home() {
       <Box height={70} />
       <Box sx={{ display: "flex" }}>
         <Sidebar />
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}></Box>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-          <Stack spacing={2} direction="row">
-            <Card sx={{ maxWidth: 345 }}>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </Card>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </Card>
-            </Stack>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Stack spacing={2} direction="row">
+                <Card sx={{ height: 194 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </Card>
+                <Card sx={{ height: 194 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Stack>
+            </Grid>
+            <Grid item xs={4}>
+              <Stack spacing={2}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <span>$5000k</span>
+                    <br />
+                    <span>Total Income</span>
+                  </CardContent>
+                </Card>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <span>$5000k</span>
+                    <br />
+                    <span>Total Income</span>
+                  </CardContent>
+                </Card>
+              </Stack>
+            </Grid>
           </Grid>
-          <Grid item xs={6}></Grid>
-        </Grid>
-        <Box height={20} />
-        <Grid container spacing={2}>
-          <Grid item xs={6} md={8}></Grid>
-          <Grid item xs={6} md={4}></Grid>
-        </Grid>
+          <Box height={20} />
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Card sx={{}}>
+                <CardContent></CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={8}>
+              <Card sx={{}}>
+                <CardContent></CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
     </>
   );
